7c2047ab1e60e8b9e4e58a6f68a4f3388dd06586
Richard <mail@richardfernando.com>
2022-09-07 11:42:40 +0530

    Beta Version 1.0

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("Javascript is working!");
+let yourScore = 0;
+let highScore = 0;
+let scoreHistory = 0;
+let timer = new Date();
+let createdTime = 0;
+let clickTimer = 0;
+loadTime = timer.getTime();
+
+
+function makeBox() {
+
+    var time=Math.random();
+
+    time=time*5000;
+
+       setTimeout(function() {
+
+
+// Show Click Me
+document.getElementById('click-btn').style.display = "flex";
+
+           createdTime=Date.now();
+           console.log(createdTime);
+
+       }, time);
+
+}
+
+function ClickFunction() {
+    // console.log(createdTime);
+
+    let clickTimer = Date.now();
+
+
+
+    // clickTime = clickTimer.getTime();
+
+
+    yourScore = clickTimer - createdTime;
+
+    let seconds = ((yourScore % 60000) / 1000).toFixed(2);
+
+
+    // console.log(yourScore);
+
+
+    document.getElementById('your-score-time').innerHTML = seconds + "s";
+
+    // Hide Click Me
+document.getElementById('click-btn').style.display = "none";
+
+makeBox();
+
+
+}
