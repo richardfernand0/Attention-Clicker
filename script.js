ce4fc9723a6df70a6355bb20caee1b1b735333a0
Richard <mail@richardfernando.com>
2022-09-08 09:33:03 +0530

    Fixed Bugs

--- script.js
+++ script.js
@@ -4,45 +4,35 @@ let scoreHistory = 0;
 let timer = new Date();
 let createdTime = 0;
 let clickTimer = 0;
+let correctClick = false;
+let wrongClick = false;
 loadTime = timer.getTime();
+
 
 
 function makeBox() {
 
-    var time=Math.random();
+    var time = Math.random();
 
-    time=time*5000;
+    time = time * 5000;
 
-       setTimeout(function() {
+    setTimeout(function () {
 
+        // Show Click Me
+        document.getElementById('click-btn').style.display = "flex";
+        createdTime = Date.now();
 
-// Show Click Me
-document.getElementById('click-btn').style.display = "flex";
-
-           createdTime=Date.now();
-           console.log(createdTime);
-
-       }, time);
+    }, time);
 
 }
 
 function ClickFunction() {
-    // console.log(createdTime);
 
     let clickTimer = Date.now();
-
-
-
-    // clickTime = clickTimer.getTime();
-
 
     yourScore = clickTimer - createdTime;
 
     let seconds = ((yourScore % 60000) / 1000).toFixed(2);
-
-
-    // console.log(yourScore);
-
 
     document.getElementById('your-score-time').innerHTML = seconds + "s";
 
@@ -56,7 +46,7 @@ function ClickFunction() {
 
         document.getElementById('message-section').innerHTML = "Congratulation you got High Score!";
 
-        setTimeout(function(){
+        setTimeout(function () {
 
             document.getElementById('message-section').innerHTML = "";
         }, 1000);
@@ -65,17 +55,11 @@ function ClickFunction() {
     };
 
 
-    // Loop HighScore
-    // for (i=0; i<highScore.length; i++) {
-    //     console.log(highScore[i]);
-    // }
-
-
-
     // Hide Click Me
-document.getElementById('click-btn').style.display = "none";
+    document.getElementById('click-btn').style.display = "none";
 
-makeBox();
-
+    makeBox();
 
 }
+
+
